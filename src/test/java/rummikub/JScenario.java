049f3141ceafbc31ceaf911cbb11c98aa2c110c6
alexeikrumshyn <alexei.w.k@gmail.com>
2022-02-10 14:53:11 -0500

    Add function to parse the methods and annotations of the step def class

--- src/test/java/rummikub/JScenario.java
+++ src/test/java/rummikub/JScenario.java
@@ -4,11 +4,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class JScenario {
     public ArrayList<String> steps;
     public String stepDefs;
+    public Hashtable<String, ArrayList<String>> methods;
 
     public JScenario(String fn, String sd) {
         steps = openFile(fn);
@@ -33,6 +35,7 @@ public class JScenario {
         Class stepDefsClass = Class.forName(this.stepDefs);
         Object obj = stepDefsClass.getDeclaredConstructor().newInstance();
 
+        parseStepDefs();
         parseSteps();
         for (String step : steps) {
             Method method = stepDefsClass.getDeclaredMethod(step, null);
@@ -54,6 +57,37 @@ public class JScenario {
                     parsedStep += "_";
             }
             steps.set(i, parsedStep);
+        }
+    }
+
+    /* Reads all methods in step defs class, loads method names and parameters with their index */
+    private void parseStepDefs() throws Exception {
+        methods = new Hashtable<>(); //<method_name, [parameters@idx]>
+
+        //loop through all declared methods in step defs class
+        for (Method m : JStepDefs.class.getMethods()) {
+            JGiven intfc = m.getAnnotation(JGiven.class);
+            if (intfc == null)
+                continue;
+
+            String cond = intfc.cond().toLowerCase();
+            String[] words = cond.split(" ");
+
+            String parsedCond = "";
+            ArrayList<String> params = new ArrayList<>();
+
+            for (int i = 0; i < words.length; ++i) {
+                if (words[i].startsWith("{") && words[i].endsWith("}")) {
+                    params.add(words[i].substring(1, words[i].length()-1) + "@"+i);
+                } else {
+                    parsedCond += words[i] + "_";
+                }
+            }
+            //trim trailing underscore if necessary
+            if (parsedCond.endsWith("_"))
+                parsedCond = parsedCond.substring(0, parsedCond.length()-1);
+
+            methods.put(parsedCond, params);
         }
     }
 
