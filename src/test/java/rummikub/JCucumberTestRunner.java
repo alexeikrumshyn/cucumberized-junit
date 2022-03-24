7ef418fa1f3d920d7abc7b1fe9f08ee1975b8c2a
alexeikrumshyn <alexei.w.k@gmail.com>
2022-03-24 15:09:20 -0400

    Add remainder of test suite in jfeature format, along with a test runner that runs them all and records the runtime

--- src/test/java/rummikub/JCucumberTestRunner.java
+++ src/test/java/rummikub/JCucumberTestRunner.java
@@ -3,6 +3,20 @@ package rummikub;
 
 public class JCucumberTestRunner {
     public static void main(String args[]) throws Exception {
+        long start = System.currentTimeMillis();
+
+        new JScenario("src/test/java/rummikub/declaring_winner.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/initial_points.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/joker_noreplacement.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/joker_replacement_invalid.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/joker_replacement_valid.jfeature", "rummikub.JStepDefs");
         new JScenario("src/test/java/rummikub/meld_testing.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/table_reuse_advanced.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/table_reuse_simple.jfeature", "rummikub.JStepDefs");
+        new JScenario("src/test/java/rummikub/user_input_validation.jfeature", "rummikub.JStepDefs");
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println(timeElapsed);
     }
 }
